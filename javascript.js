841fb0ce017c9d42445ebbc4b0f5848438719cb5
doo78 <rickydeangordon@gmail.com>
2024-09-07 18:07:44 +0100

    The functionality includes everything but the read toggle because I was unable to retrieve the radio button values when the form is submitted. Also, I didn't make it look nice. I'll come back to this.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 const myLibrary = [];
+
 const booksContainer = document.querySelector(".books-container");
 const newBookBtn = document.querySelector("#new-book-btn");
 const submitBtn = document.querySelector("#submit-btn");
+const newBookForm = document.querySelector("#new-book-form");
+const errorMessage = document.querySelector("#error-message");
+
 
 function Book(author, title, numberOfPages, read) {
   // the constructor...
@@ -15,42 +19,167 @@ function addBookToLibrary(book) {
    myLibrary.push(book);
 }
 
+function clearForm(){
+    const formValues = document.querySelectorAll("input");
+    formValues.forEach(input => {
+
+        input.value = ""
+    })
+
+    errorMessage.textContent = "";
+    newBookForm.style.display = "none";
+
+}
+
+function isLogged(author, title){
+    let logged = false;
+    myLibrary.forEach(book => {
+        if (book.author == author){
+            if (book.title == title){
+                logged = true;
+            }
+        }
+    })
+
+    return logged;
+}
+
+function isEmpty(){
+    const formValues = document.querySelectorAll("input");
+    let empty = false;
+
+    formValues.forEach(input => {
+
+        if (input.type != "radio" && input.value === ""){
+            empty = true;
+        }
+    
+    });
+
+    return empty;
+}
+
 function displayAllBooks(){
 
     myLibrary.forEach(book => {
 
         const div = document.createElement("div");
-        div.textContent = "Author: " + book.author + " Title: " + book.title + " Number of pages: " 
-        + book.numberOfPages + "Read: " + book.read;
+        div.innerHTML = "Author: " + book.author + "<br>Title: " + book.title + "<br>Number of pages: " 
+        + book.numberOfPages + "<br>Read: " + book.read + "<br>";
+
         booksContainer.appendChild(div);
 
     });
 }
 
+function displayNewBook(book){
+    const div = document.createElement("div");
+    div.innerHTML = "Author: " + book.author + "<br>Title: " + book.title + "<br>Number of pages: " 
+    + book.numberOfPages;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+
+    const toggleReadBtn = document.createElement("button");
+    toggleReadBtn.textContent = "Read";
+
+    div.appendChild(removeBtn);
+    div.appendChild(toggleReadBtn);
+
+    booksContainer.appendChild(div);
+
+    removeBtn.addEventListener("click", () => {
+        // Remove the book from the DOM
+        booksContainer.removeChild(div);
+
+        // Find the index of the book in the myLibrary array and remove it
+        const index = myLibrary.indexOf(book);
+        
+        myLibrary.splice(index, 1);  // Remove the book from the array
+        
+    });
+
+    toggleReadBtn.addEventListener("click", () => {
+        console.log(book.read);
+        if (book.read === "Not read"){
+            book.read = "read";
+            toggleReadBtn.textContent = "Unread";
+        }
+
+        else{
+            book.read = "Not read";
+            toggleReadBtn.textContent = "Read";
+        }
+        
+    });
+}
+
+function displayErrorMessage(error){
+
+    if (error == "logged"){
+        errorMessage.textContent = "Book is already in library";
+    }
+
+    else if (error == "full"){
+        errorMessage.textContent = "Library is full";
+    }
+    
+    else if (error == "empty"){
+        errorMessage.textContent = "Please fill out all the data"
+    }
+}
+
 newBookBtn.addEventListener("click", () => {
-    const newBookForm = document.querySelector("#new-book-form");
-    newBookForm.style.display = "block";
+
+    let temp;
+
+    if (newBookForm.style.display != "none"){
+        temp = "none";
+        clearForm();
+    }
+
+    else if (newBookForm.style.display != "block"){
+        temp = "block";
+    }
+
+    newBookForm.style.display = temp;
+
   });
 
 submitBtn.addEventListener("click", () => {
     const author = document.querySelector("#author").value;
     const title = document.querySelector("#title").value;
-    const numberOfPages = document.querySelector("#number-of-pages").value;
 
-    let readRadios = document.getElementsByName("read");
-    let readStatus;
+    if (myLibrary.length < 5){
+        if (!isLogged(author, title)){
+            if (!isEmpty()){
+                const numberOfPages = document.querySelector("#number-of-pages").value;
 
-    readRadios.forEach(radio => {
-        if (radio.checked) {
-            readStatus = radio.value;  // This will be either 'read' or 'not-read'
+      
+
+
+                const newBook = new Book (author, title, numberOfPages, "read");
+                addBookToLibrary(newBook);
+
+                displayNewBook(newBook);
+
+                clearForm();
+            }
+            
+            else{
+                displayErrorMessage("empty");
+            }
         }
-    });
 
+        else{
+            displayErrorMessage("logged");
+        }
+    }
 
-    const newBook = new Book (author, title, numberOfPages, readStatus);
-    addBookToLibrary(newBook);
-    displayAllBooks();
-    console.log(myLibrary);
+    else{
+        displayErrorMessage("full");
+    }
+    
 });
 
 submitBtn.addEventListener('click', function(event) {
